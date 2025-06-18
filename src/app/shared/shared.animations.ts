35ec0926f33df2a1fae28b8ad18fd93f2f46f29f
ilya <riper752@gmail.com>
2025-06-18 18:39:22 +0300

    fix(anim): исправлено поведение анимации на адаптивной версии

--- src/app/shared/shared.animations.ts
+++ src/app/shared/shared.animations.ts
@@ -22,7 +22,15 @@ export const overlayAnimation = trigger('transformPanel', [
 export const expandAnimation = trigger('expandFade', [
   transition(':enter', [
     style({ opacity: 0, height: '0', marginBottom: '0', padding: '0' }),
-    animate('300ms ease-out', style({ opacity: 1, height: '*', marginBottom: '32px', padding: '12px 16px' })),
+    animate(
+      '300ms ease-out',
+      style({
+        opacity: 1,
+        height: '*',
+        marginBottom: 'var(--selection-bar-animation-margin-bottom, 32px)',
+        padding: '12px 16px',
+      }),
+    ),
   ]),
   transition(':leave', [animate('300ms ease-in', style({ opacity: 0, height: '0', marginBottom: '0', padding: '0' }))]),
 ]);
