ebf0f80bbc2e5c0ee601257bbd98aafb4397a5c0
ilya <riper752@gmail.com>
2025-08-20 17:25:31 +0300

    feat(links): добавлены ссылки для profile-info-overlay

--- src/app/modules/auth/components/profile-info-overlay/profile-info-overlay.constants.ts
+++ src/app/modules/auth/components/profile-info-overlay/profile-info-overlay.constants.ts
@@ -1,7 +1,6 @@
 export const ProfileInfoOverlayLocalization = {
   aboutService: $localize`:@@profileInfoOverlay.aboutService:About service`,
   versionHistory: $localize`:@@profileInfoOverlay.versionHistory:Version history`,
-  feedback: $localize`:@@profileInfoOverlay.feedback:Feedback`,
   help: $localize`:@@profileInfoOverlay.help:Help`,
   oldApp: $localize`:@@profileInfoOverlay.oldApp:Old app`,
 } as const;
