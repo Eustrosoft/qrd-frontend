99fe243d71c028cd1851275070ff0c769ef58f25
ilya <riper752@gmail.com>
2025-08-25 18:03:16 +0300

    fix(selection): исправлена ошибка при выборе нескольких записей QR карточек

--- src/app/pages/qr-cards/components/qr-card-list/qr-card-list.component.ts
+++ src/app/pages/qr-cards/components/qr-card-list/qr-card-list.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, DestroyRef, effect, inject, OnInit } from '@angular/core';
 import { createDispatchMap, createSelectMap } from '@ngxs/store';
 import { QrCardsState } from '@app/pages/qr-cards/state/qr-cards.state';
-import { DeleteQrCards, FetchQrCardList } from '@app/pages/qr-cards/state/qr-cards.actions';
+import { DeleteQrCards, FetchQrCardList, SetSelectedQrCards } from '@app/pages/qr-cards/state/qr-cards.actions';
 import { ViewListItemComponent } from '@shared/components/view-list-item/view-list-item.component';
 import { SelectionModel } from '@angular/cdk/collections';
 import { ToHexPipe } from '@shared/pipe/to-hex.pipe';
@@ -11,8 +11,8 @@ import { SharedLocalization } from '@shared/shared.constants';
 import { QrCardsLocalization } from '@app/pages/qr-cards/qr-cards.constants';
 import { UiSkeletonComponent } from '@ui/ui-skeleton/ui-skeleton.component';
 import { MatIcon } from '@angular/material/icon';
-import { outputFromObservable } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, startWith } from 'rxjs';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { UiSidenavService } from '@ui/ui-sidenav/ui-sidenav.service';
 import { IS_SMALL_SCREEN, IS_XSMALL } from '@cdk/tokens/breakpoint.tokens';
@@ -67,6 +67,7 @@ export class QrCardListComponent implements OnInit {
 
   protected readonly actions = createDispatchMap({
     fetchQrCards: FetchQrCardList,
+    setSelectedQrCards: SetSelectedQrCards,
     deleteQrCards: DeleteQrCards,
   });
 
@@ -74,9 +75,18 @@ export class QrCardListComponent implements OnInit {
   protected readonly skeletonHeight = computed(() => this.isSmallScreen() ? '96' : '144');
 
   protected readonly selectionModel = new SelectionModel(true, this.selectors.selectedQrCardList());
-  public readonly selectionChanged = outputFromObservable(
-    this.selectionModel.changed.asObservable().pipe(map(() => this.selectionModel.selected)),
+  public readonly selectionChanged = toSignal(
+    this.selectionModel.changed.asObservable().pipe(
+      map(() => this.selectionModel.selected),
+      startWith<number[]>([]),
+    ),
+    { requireSync: true },
   );
+
+  private readonly selChangeEff = effect(() => {
+    const selection = this.selectionChanged();
+    this.actions.setSelectedQrCards(selection);
+  });
 
   private readonly selEff = effect(() => {
     const selectedValues = this.selectors.selectedQrCardList();
