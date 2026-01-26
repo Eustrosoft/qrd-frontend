c6054dbc9d6c55168ff548d326df1134f22ab91c
ilya <riper752@gmail.com>
2026-01-26 18:58:58 +0300

    fix(p-code): удален guard

--- src/app/pages/p-codes/p-codes.routes.ts
+++ src/app/pages/p-codes/p-codes.routes.ts
@@ -1,13 +1,10 @@
-import { GuardResult, RedirectCommand, Router, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { RouteTitles } from '@shared/shared.constants';
 import { AppRoutes } from '@app/app.constants';
 import { unsavedDataGuard } from '@shared/guards/unsaved-data.guard';
 import { PCodesComponent } from '@app/pages/p-codes/p-codes.component';
 import { PCodeEditComponent } from '@app/pages/p-codes/components/p-code-edit/p-code-edit.component';
 import { pCodeFormResolver } from '@app/pages/p-codes/resolvers/p-code-form.resolver';
-import { select } from '@ngxs/store';
-import { QrCardsSelectors } from '@app/pages/qr-cards/state/qr-cards.selectors';
-import { inject } from '@angular/core';
 
 export const pCodesRoutes: Routes = [
   {
@@ -19,18 +16,6 @@ export const pCodesRoutes: Routes = [
         title: RouteTitles.newPCode,
         component: PCodeEditComponent,
         resolve: { pCodeForm: pCodeFormResolver() },
-        canActivate: [
-          (): GuardResult => {
-            const router = inject(Router);
-            const pCodesState = select(QrCardsSelectors.getPCodesState$);
-            if (pCodesState().pCodeList.length) {
-              return true;
-            }
-            return new RedirectCommand(
-              router.currentNavigation()?.previousNavigation?.extractedUrl ?? router.createUrlTree([AppRoutes.qrCards]),
-            );
-          },
-        ],
       },
       {
         path: `:rowId/${AppRoutes.edit}`,
